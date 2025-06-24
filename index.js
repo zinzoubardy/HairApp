80cfec26e81e80182762e173ca62c9d4eabecea9
zinzoubardy <zinzoubardy@gmail.com>
2025-06-25 00:58:09 +0100

    package.json update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { registerRootComponent } from 'expo';
 import App from './src/App';
-import 'react-native-url-polyfill/auto';
+//import 'react-native-url-polyfill/auto';
 import 'react-native-gesture-handler';
 registerRootComponent(App);
